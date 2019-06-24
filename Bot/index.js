77574f87b021020598e4162ff75b9113d972bd5b
SamriJek <37089465+SamriJek@users.noreply.github.com>
2019-06-24 13:32:09 -0400

    Update index.js

---
+++ Bot/index.js
@@ -0,0 +1,14 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', ()  =>{
+    //console.log('I am ready!');
+    client.user.setStatus("Online");
+    client.user.setActivity("m!help|Monogolf")
+});
+
+client.on('message', message => {
+
+
+});
+client.login(process.env.BOT_TOKEN);
